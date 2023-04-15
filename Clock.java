6b6bace52a3575398765456dc1294c6492d3ab85
bisratdbelay <bisratdb@outlook.com>
2023-04-15 17:50:20 -0500

    v 2.00

---
+++ Clock.java
@@ -0,0 +1,13 @@
+package head_first_java;
+
+class Clock {
+	String time;
+	
+	void setTime(String t) {
+		time = t;
+	}
+	
+	String getTime() {
+		return time;
+	}
+}
---
+++ XCopy.java
@@ -0,0 +1,18 @@
+package head_first_java;
+
+class XCopy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int orig = 42;
+		XCopy x = new XCopy();
+		int y = x.go(orig);
+		System.out.println(orig + " " + y);
+		
+	}
+	
+	int go(int arg) {
+		arg = arg * 2;
+		return arg;
+	}
+}
